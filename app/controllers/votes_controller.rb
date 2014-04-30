c5fbd816ccc2a35f9565a8b2a4ba6b8f028e9341
Garrett Calbert <garrettcalbert@gmail.com>
2014-04-30 06:23:59 -0700

    No multiple voting per story

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,11 +7,18 @@ class VotesController < ApplicationController
 		@story = Story.find(params[:story_id])
 
 		# make a new vote on this story
-		@vote = @story.votes.new 
+		@vote = @story.votes.new
 
-		@vote.save
+		# save the ip address to the vote
+		@vote.ip_address = request.ip
 
-		flash[:success] = "You have voted this story up"
+		if @vote.save
+
+			flash[:success] = "You have voted this story up"
+		else
+			# the error part doesnt mean anything, you can put whatever
+			flash[:error] = "You can't vote up twice"
+		end
 
 		redirect_to story_path(@story)
 	end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,8 @@ class Vote < ActiveRecord::Base
 	# association
 	belongs_to :story, counter_cache: true
 	
+	# validation to make sure stories cant be voted
+	# multiple times
+	validates :ip_address, uniqueness: { scope: :story_id }
+
 end
